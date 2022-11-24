f84ab1724f07b154037f27e1ae136923ec234922
Sankarshan Ghosh <sankarshanghosh19@gmail.com>
2022-11-24 12:09:24 +0530

    practice

--- Practice/test.py
+++ Practice/test.py
@@ -1,24 +1,24 @@
+s = "ab"
+t = "baab"
+map = {}
 
-List = [2, 2, 0, 0, 1, 1, 1]
-class Solution:
-    def sortColors(self, nums: List[int]) -> None:
-        """
-        Do not return anything, modify nums in-place instead.
-        """
-        l = 0 
-        r = len(nums) - 1
-        i = 0
 
-        def swap(i, j):
-            temp = nums[i]
-            nums[i] = nums[j]
-            nums[j] = temp
-        while i <= r:
-            if nums[i] == 0:
-                swap(l, i)
-                l += 1
-            elif nums[i] == 2:
-                swap(i, r)
-                r -= 1
-                i -= 1
-            i += i
+def solution():
+    temp = None
+    for i, c in enumerate(t):
+        if c in map:
+            None
+        else:
+            map[c] = i
+    return map
+    # for c in s:
+    #     if c in map and temp is None:
+    #         temp = map[c]
+    #     elif c in map and map[c] > temp:
+    #         temp = map[c]
+    #     else:
+    #         return False
+    # return True
+
+
+print(solution())
