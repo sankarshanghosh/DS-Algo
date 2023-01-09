eef81ac113ad3446d2afe17876a356f8df78f4a1
Sankarshan Ghosh <sankarshanghosh19@gmail.com>
2023-01-09 10:52:18 +0530

    update

--- Practice/test.py
+++ Practice/test.py
@@ -1,24 +1,3 @@
-s = "ab"
-t = "baab"
-map = {}
+media = "007"
 
-
-def solution():
-    temp = None
-    for i, c in enumerate(t):
-        if c in map:
-            None
-        else:
-            map[c] = i
-    return map
-    # for c in s:
-    #     if c in map and temp is None:
-    #         temp = map[c]
-    #     elif c in map and map[c] > temp:
-    #         temp = map[c]
-    #     else:
-    #         return False
-    # return True
-
-
-print(solution())
+print(int(media))
